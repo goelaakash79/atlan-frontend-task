e0a66e4302829416ecf5503d0038f0f93a858e8a
Aakash Goel <goelaakash79@gmail.com>
2020-07-01 23:05:51 +0530

    --fix: deploying...

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React from "react";
-import Header from "./components/Header";
-import PrimaryMain from "./components/PrimaryMain";
-import RunsChart from "./components/RunsChart";
-import Footer from "./components/Footer";
+// import Header from "./components/Header";
+// import PrimaryMain from "./components/PrimaryMain";
+// import RunsChart from "./components/RunsChart";
+// import Footer from "./components/Footer";
+
+const PrimaryMain = React.lazy(() => import("./components/PrimaryMain.js"));
+const Header = React.lazy(() => import("./components/Header.js"));
+const RunsChart = React.lazy(() => import("./components/RunsChart.js"));
+const Footer = React.lazy(() => import("./components/Footer.js"));
 
 function App() {
 	return (
-		<>
+		<React.Suspense fallback={<div>loading ...</div>}>
 			<Header />
 			<PrimaryMain />
 			<RunsChart />
 			<Footer />
-		</>
+		</React.Suspense>
 	);
 }
 
